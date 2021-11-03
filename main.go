0e334d46c536098c66b67e79950bfee68fb31175
a.pak <2ezmmr@gmail.com>
2021-11-03 11:40:40 +0300

    comms

--- main.go
+++ main.go
@@ -13,13 +13,12 @@ import (
 	"strconv"
 )
 
-
 type queueInfo struct {
-	FileN   string `json:"fileN"`
-	StringN int    `json:"stringN"`
+	FileN   string `json:"fileN"`   //номер файла для указателя
+	StringN int    `json:"stringN"` //порядковый номер символа для указателя
 }
 
-func popInfo(f *os.File) (string, int) {
+func popInfo(f *os.File) (string, int) { //функция получает номер файла и порядковый номер символа для указателя из файлов
 	fi, _ := f.Stat()
 	buf := bytes.NewBuffer(make([]byte, 0, fi.Size()))
 	f.Seek(0, io.SeekStart)
@@ -34,11 +33,11 @@ func popInfo(f *os.File) (string, int) {
 
 }
 
-func Enqueue(fil *os.File, value string) {
+func Enqueue(fil *os.File, value string) { //добавляет запись в конец очереди
 	filename, stringname := popInfo(fil)
 	var f *os.File
-	f, err := os.OpenFile(filename + ".json", os.O_RDWR, 0644)
-	if errors.Is(err, os.ErrNotExist) {
+	f, err := os.OpenFile(filename+".json", os.O_RDWR, 0644) //открываем файл, имя которого получаем из файла с информацией для указателя
+	if errors.Is(err, os.ErrNotExist) {                      //если такого файла еще не существует, создаем и заполняем пустыми байтами
 		size := int64(1093)
 		f, err := os.Create(filename + ".json")
 		if err != nil {
@@ -58,11 +57,9 @@ func Enqueue(fil *os.File, value string) {
 		}
 	}
 
-
-
 	defer f.Close()
 
-	mmap, _ := mmap.Map(f, mmap.RDWR, 0 )
+	mmap, _ := mmap.Map(f, mmap.RDWR, 0) //отображаем файл в память
 
 	defer mmap.Unmap()
 	var ptr int
@@ -71,9 +68,9 @@ func Enqueue(fil *os.File, value string) {
 	xname, _ = strconv.Atoi(filename)
 	ptr = stringname
 
-	if stringname + len(value) > len(mmap) {
-		//mmap[stringname] = 'g'
+	if stringname+len(value) > len(mmap) { //проверяем, хватает ли места в файле для записи
 
+		//если да, то создаем новый файл, и передаем информацию указателю
 		xname, _ = strconv.Atoi(filename)
 		f.Close()
 		xname += 1
@@ -100,15 +97,15 @@ func Enqueue(fil *os.File, value string) {
 		}
 	}
 
-	for i := 0; i < len(value); i++{
+	for i := 0; i < len(value); i++ { //добавляем запись в файл
 
-		mmap[stringname + i] = value[i]
+		mmap[stringname+i] = value[i]
 		ktr = i + 1
 	}
 	ptr = ptr + ktr
 	ktr = 0
 	mmap.Flush()
-	os.Truncate("writeQueue.json", 0)
+	os.Truncate("writeQueue.json", 0) //обновляем файл для указателя
 	newInf := queueInfo{}
 	newInf.FileN = strconv.Itoa(xname)
 	newInf.StringN = ptr
@@ -119,27 +116,25 @@ func Enqueue(fil *os.File, value string) {
 	mmap.Unmap()
 }
 
+func Dequeue(fil *os.File) (el string) { //функция для получения первой в очереди записи
+	filename, stringname := popInfo(fil) //получаем информацию для указателя
 
+	f, _ := os.OpenFile(filename+".json", os.O_RDWR, 0644)
 
-func Dequeue(fil *os.File) (el string) {
-	filename, stringname := popInfo(fil)
-
-	f, _ := os.OpenFile(filename + ".json", os.O_RDWR, 0644)
-
-	mmap, _ := mmap.Map(f, mmap.RDWR, 0 )
+	mmap, _ := mmap.Map(f, mmap.RDWR, 0) //отображаем файл в память
 	defer mmap.Unmap()
-	strlen, _ := strconv.Atoi(string(mmap[stringname:stringname+2]))
-	if strlen == 0 {
+	strlen, _ := strconv.Atoi(string(mmap[stringname : stringname+2])) //читаем первые два байта, чтобы определить размер записи
+	if strlen == 0 {                                                   //если размер 0, то  записей в очереди нет
 		fmt.Println("Нет записей в очереди")
 	}
 
-	el = string(mmap[stringname + 2:stringname + 2 + strlen])
+	el = string(mmap[stringname+2 : stringname+2+strlen]) //получаем саму запись
 
-	stringname = stringname + 2 + strlen
+	stringname = stringname + 2 + strlen //продвигаем указатель вперед на длину записи
 	var xname int
 	xname, _ = strconv.Atoi(filename)
 	f.Close()
-	if  (mmap[stringname]) == '\x00' {
+	if (mmap[stringname]) == '\x00' { //если после записи идет пустой байт, то переходим на следующий файл и удаляем предыдущий
 		mmap.Unmap()
 
 		e := os.Remove(strconv.Itoa(xname) + ".json")
@@ -152,7 +147,7 @@ func Dequeue(fil *os.File) (el string) {
 
 	}
 	mmap.Flush()
-	os.Truncate("readQueue.json", 0)
+	os.Truncate("readQueue.json", 0) //обновляем информацию в файле для указателя
 	newInf := queueInfo{}
 	newInf.FileN = strconv.Itoa(xname)
 	newInf.StringN = stringname
@@ -167,7 +162,7 @@ func Dequeue(fil *os.File) (el string) {
 
 func main() {
 
-	if _, err := os.Stat("0.json"); errors.Is(err, os.ErrNotExist) {
+	if _, err := os.Stat("0.json"); errors.Is(err, os.ErrNotExist) { //проверяем на наличие первого файла, если его не существует то создаем
 		size := int64(2011)
 		fd, err := os.Create("0.json")
 		if err != nil {
@@ -188,9 +183,7 @@ func main() {
 		fd.Close()
 	}
 
-
-
-
+	//проверяем на наличие файла с информацией для указателя чтения, если его не существует то создаем
 	fileQueueRead, err := os.OpenFile("readQueue.json", os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		log.Fatalf("open file error: %v", err)
@@ -205,7 +198,7 @@ func main() {
 		err = ioutil.WriteFile("readQueue.json", newInfM, 0644)
 	}
 	defer fileQueueRead.Close()
-
+	//проверяем на наличие файла с информацией для указателя записи, если его не существует то создаем
 	fileQueueWrite, err := os.OpenFile("writeQueue.json", os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		log.Fatalf("open file error: %v", err)
